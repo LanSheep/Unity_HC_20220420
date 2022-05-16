2dfc9e6491f3667840c54a8bae7610852ae170ab
LanSheep <60056925+LanSheep@users.noreply.github.com>
2022-05-16 21:33:08 +0800

    彈珠軌跡與程式資料認識

---
+++ Assets/Script/LearnData.cs
@@ -0,0 +1,34 @@
+//2022.05.16 綿羊
+/*多行註解
+ * 藍色-保留字
+ * 白色-名稱
+ * 綠色-資料類別
+ */
+using UnityEngine;
+
+//修飾詞 類別(藍圖) 類別名稱 繼承(物件導向設計 OOP三大原則)
+public class LearnData : MonoBehaviour
+{
+    //LearnData 類別成員區域
+
+    /*儲存資料方式：
+      欄位 field
+      儲存各種資料
+      欄位語法
+      資料類型 欄位自訂名稱 結束符號
+      定義一筆整數資料名稱叫做hp
+     */
+    //資料類型：四大基本類型
+    //整數：保存正負沒有小數點的資料 int
+    //浮點數：保存正負有小數點的資料 float
+    //字串：保存文字資訊 string
+    //布林值：保存有、沒有 bool
+
+    int hp;
+    int lv;
+    float exp;
+    bool PlayersName;
+    bool hasSkill;
+    //書籍參考：Clean Code 無瑕的程式
+}
+  //非LearnData 類別成員區域
